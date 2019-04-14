0fbde6cd9617c5186182bcad228ebc5bc36481f8
Annabel Treshansky <annabelt@Annabels-MacBook-Air.local>
2019-04-14 22:12:20 +0100

    using javascript to toggle display of search form

--- src/app.js
+++ src/app.js
@@ -11,6 +11,25 @@ const SelectView = require('./views/select_view.js');
 
 document.addEventListener('DOMContentLoaded', () => {
   console.log('JavaScript Loaded');
+  const searchForm = document.querySelector('#search-form');
+  const selectView = new SelectView(searchForm);
+  const showFormInput = document.querySelector('#show-form');
+
+  const handleShowFormInput = function(event){
+      let showForm = false;
+
+      if (event.target.checked){
+        showForm=true;
+        searchForm.className="show";
+      } else {
+        showForm = false;
+        searchForm.className="hide";
+
+      }
+      console.log('showForm', showForm);
+  };
+  showFormInput.addEventListener('change', handleShowFormInput);
+
   var apiURL = 'https://api.publicapis.org/entries';
   const infoProcessor = new InfoProcessor();
   infoProcessor.fetchURLData(apiURL);
---
+++ src/views/select_view.js
@@ -0,0 +1,21 @@
+const PubSub = require('../helpers/pub_sub.js');
+
+const SelectView = function(container) {
+  this.container = container;
+};
+
+SelectView.prototype.handleShowFormInput = function(event){
+    let showForm = false;
+
+    if (event.target.checked){
+      showForm=true;
+      this.container.className="show";
+    } else {
+      showForm = false;
+      this.container.className="hide";
+
+    }
+    console.log('showForm', showForm);
+};
+
+module.exports = SelectView;
